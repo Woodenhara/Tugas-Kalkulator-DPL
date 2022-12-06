72fdd5300e2bbe8a5019b279cb52bd048e13aab2
Althaf Ramadan Kesatria Nova <118158281+Woodenhara@users.noreply.github.com>
2022-12-06 13:32:58 +0700

    Menambah tampilan kalkulator dan memperindah output
    Menambahkan tampilan kalkulator, memperindah tampilan output untuk memilih metode, dan menambah console.clear untuk membersihkan setiap kali menggunakan atau menampilkan output

--- Kalkulator.js
+++ Kalkulator.js
@@ -1,8 +1,18 @@
 var input = require('readline-sync');
+console.clear()
 no1 = input.questionInt("input nomor pertama: ");
 no2 = input.questionInt("input nomor kedua: ");
-console.log("1. Penjumlahan")
-console.log("2. Pengurangan")
-console.log("3. Perkalian")
-console.log("4. Pembagian")
+
+console.log("██╗  ██╗██╗███████╗ █████╗  █████╗ ██████╗ ██╗██╗   ██╗ █████╗ ██╗  ██╗ █████╗ ")
+console.log("╚██╗██╔╝██║██╔════╝██╔══██╗██╔══██╗██╔══██╗██║╚██╗ ██╔╝██╔══██╗██║  ██║██╔══██╗")
+console.log(" ╚███╔╝ ██║███████╗███████║███████║██████╔╝██║ ╚████╔╝ ███████║███████║███████║")
+console.log(" ██╔██╗ ██║╚════██║██╔══██║██╔══██║██╔══██╗██║  ╚██╔╝  ██╔══██║██╔══██║██╔══██║")
+console.log("██╔╝ ██╗██║███████║██║  ██║██║  ██║██████╔╝██║   ██║   ██║  ██║██║  ██║██║  ██║")
+console.log("╚═╝  ╚═╝╚═╝╚══════╝╚═╝  ╚═╝╚═╝  ╚═╝╚═════╝ ╚═╝   ╚═╝   ╚═╝  ╚═╝╚═╝  ╚═╝╚═╝  ╚═╝")
+    console.log("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+    console.log("         1. Penjumlahan           ")
+    console.log("         2. Pengurangan           ")
+    console.log("         3.  Perkalian            ")
+    console.log("         4.  Pembagian            ")
+    console.log("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
 input.requestionlnt("pilih metode yang kamu inginkan : ")
