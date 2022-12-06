80f060477363278c4b20c364ff1b41f28289484b
anemratnasari <118803025+anemratnasari@users.noreply.github.com>
2022-12-06 13:29:09 +0700

    Menambah inputan angka dan metode
    Menambahkan inputan untuk angka pertama dan angka kedua, lalu inputan untuk memilih metode

--- Kalkulator.js
+++ Kalkulator.js
@@ -1,1 +1,8 @@
-
+var input = require('readline-sync');
+no1 = input.questionInt("input nomor pertama: ");
+no2 = input.questionInt("input nomor kedua: ");
+console.log("1. Penjumlahan")
+console.log("2. Pengurangan")
+console.log("3. Perkalian")
+console.log("4. Pembagian")
+input.requestionlnt("pilih metode yang kamu inginkan : ")
