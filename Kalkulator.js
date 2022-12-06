1224878295e435f2bbfe2705ce168040fb3bc73e
Althaf Ramadan Kesatria Nova <118158281+Woodenhara@users.noreply.github.com>
2022-12-06 14:44:51 +0700

    Memperbaiki error
    Memperbaiki error dimana kode program tertulis input.requestionLnt yang dimana seharusnya menggunakan variabel diawal dan tertulis seperti berikut :
    var metode = input.questionInt

--- Kalkulator.js
+++ Kalkulator.js
@@ -1,7 +1,7 @@
 var input = require('readline-sync');
 console.clear()
-no1 = input.questionInt("input nomor pertama: ");
-no2 = input.questionInt("input nomor kedua: ");
+no1 = input.questionInt("Input bilangan pertama: ");
+no2 = input.questionInt("Input bilangan kedua: ");
 
 console.log("██╗  ██╗██╗███████╗ █████╗  █████╗ ██████╗ ██╗██╗   ██╗ █████╗ ██╗  ██╗ █████╗ ")
 console.log("╚██╗██╔╝██║██╔════╝██╔══██╗██╔══██╗██╔══██╗██║╚██╗ ██╔╝██╔══██╗██║  ██║██╔══██╗")
@@ -15,7 +15,7 @@ console.log("╚═╝  ╚═╝╚═╝╚══════╝╚═╝  ╚
     console.log("         3.  Perkalian            ")
     console.log("         4.  Pembagian            ")
     console.log("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
-input.requestionlnt("pilih metode yang kamu inginkan : ")
+var metode = input.questionInt("Pilih metode yang kamu inginkan : ")
 
 if (metode == 1) {
     console.log("Hasilnya adalah" , no1 + no2);
